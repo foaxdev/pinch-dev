b74ad8ba823216a6c1c651f4fb7a230971281937
Irina Kovalchuk <synthbeing@icloud.com>
2019-09-17 22:14:21 +0300

    Internet Explorer 11 fixes

--- gulpfile.js
+++ gulpfile.js
@@ -73,7 +73,7 @@ gulp.task("images", function () {
 });
 
 gulp.task("webp", function () {
-  return gulp.src("source/img/*.{png}")
+  return gulp.src("source/img/**/*.{png}")
     .pipe(webp({quality: 90}))
     .pipe(gulp.dest("source/img"));
 });
